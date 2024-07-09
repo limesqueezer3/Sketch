5bd48f7124ca1d79090163b92f47cb8d015d0e46
Charlie <charliehu2808@outlook.com>
2024-07-09 02:04:56 +0200

    it works

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 const container = document.querySelector(".Container")
+const CONTAINER_WIDTH = 800;
+const CONTAINER_HEIGHT = 800;
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+button.addEventListener("click", () => {
+    deleteChild(container);
+    row = Math.min(input.value, 100);
+    console.log(row);
+    setup();
+})
 
-for (let i = 0; i < 16; i++) {
-    row = document.createElement("row");
-    container.appendChild(row);
-    row.style.display = "flex";
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement("div");
-        div.textContent = "hello";
-        row.appendChild(div);
+container.style.width = `${CONTAINER_WIDTH}px`;
+container.style.height = `${CONTAINER_HEIGHT}px`;
+let row = 16;
+setup();
+
+
+function setup () {
+    for (let i = 0; i < row*row; i++) {
+        let square = document.createElement("div");
+        square.style.borderColor = "BLACK";
+        square.style.height = `${CONTAINER_WIDTH/row}px`;
+        square.style.width = `${CONTAINER_WIDTH/row}px`;
+        square.style.borderStyle = "solid";
+        square.addEventListener("mouseover", (e => {
+            e.target.style.backgroundColor = "BLACK";
+        }))
+        container.appendChild(square);
+    }
+}
+
+function deleteChild(e) {
+    //e.firstElementChild can be used. 
+    let child = e.lastElementChild;
+    while (child) {
+        e.removeChild(child);
+        child = e.lastElementChild;
     }
 }
 
